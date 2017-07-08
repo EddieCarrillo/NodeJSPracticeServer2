7a27ba54ce1a50c2301e6663413fb19f85ddb19a
EddieCurio <edcarril@ucsd.edu>
2017-07-08 11:23:42 -0700

    First commit

--- server/server.js
+++ server/server.js
@@ -18,8 +18,63 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
+
+
 var lions = [];
 var id = 0;
+
+app.get("/lions", function(req, res){
+	res.status(200);
+    res.json(lions);
+
+});
+
+app.get("/lions/:id", function(req, res){
+	var params = req.params;
+	var id = params.id;
+	res.status(200)
+	res.json(lions[id]);
+	
+});
+
+app.post("/lions", function(req, res){
+	var newLion = req.body;
+	lions.push(newLion);
+	res.status(201);
+	res.json(newLion);
+	
+
+});
+
+app.put("lions/:id", function(req, res){
+    var update = req.body
+    if (update.id){
+    	delete update.id
+    }
+
+    var lionIndex = _.findIndex(lions, {id: req.params.id});
+    if (!lions[lionIndex]){//If we can't find the lion ...
+    	res.send()
+    }else{
+    	//Merging the objects the update and lions[lionIndex] and return lions[lionIndex]
+    	var updatedLion = _.assign(lions[lionIndex], update);
+    	res.json();
+    }
+
+});
+
+app.delete("lions/:id", function(req, res){
+   var lionIndex = _.findIndex(lions, {id: req.params.id});
+   if (!lions[lionIndex]){//If we cannot find the lion 
+   	res.send();
+   }else{
+   	var deletedLion = lions[lionIndex];
+   	lions.splice(lionIndex, 1);
+   	res.json(deletedLion);
+   }
+
+
+});
 
 // TODO: make the REST routes to perform CRUD on lions
 
